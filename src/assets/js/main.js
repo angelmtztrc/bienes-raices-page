c4fe960a928ac2db8a644513a332d16cdbfbc6a0
Angel Martinez <angel.martineztrc@gmail.com>
2021-03-31 22:14:15 -0600

    feat(src/js): :sparkles:  add scroll-reveal effect

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,15 @@
+import ScrollReveal from 'scrollreveal';
 
+const elementsArray = [
+  document.querySelector('#about-us'),
+  document.querySelector('#latest-announcements'),
+  document.querySelector('#call-to-action'),
+  document.querySelector('#blog-testimonials'),
+  document.querySelector('#footer')
+];
+
+ScrollReveal().reveal(elementsArray, {
+  distance: '100%',
+  origin: 'bottom',
+  delay: 300
+});
